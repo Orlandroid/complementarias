f5bf0ce6301f474629ee6f740c205b82c4098510
Orlandroid <lopez.orlando.gold9@gmail.com>
2024-01-22 12:57:17 -0600

    Upadate libraries

--- app/src/main/java/com/example/app_004_complementariasmx/data/db/ComplementDatabase.kt
+++ app/src/main/java/com/example/app_004_complementariasmx/data/db/ComplementDatabase.kt
@@ -5,10 +5,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.app_004_complementariasmx.data.db.dao.RaceDao
 import com.example.app_004_complementariasmx.data.db.dao.UserDao
+import com.example.app_004_complementariasmx.data.db.entities.Race
 import com.example.app_004_complementariasmx.data.db.entities.User
 
 
-@Database(entities = [User::class, RaceDao::class], version = 1, exportSchema = false)
+@Database(entities = [User::class, Race::class], version = 2, exportSchema = false)
 
 abstract class ComplementDatabase : RoomDatabase() {
 
--- app/src/main/java/com/example/app_004_complementariasmx/data/db/dao/RaceDao.kt
+++ app/src/main/java/com/example/app_004_complementariasmx/data/db/dao/RaceDao.kt
@@ -1,9 +1,11 @@
 package com.example.app_004_complementariasmx.data.db.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.app_004_complementariasmx.data.db.entities.Race
 
+@Dao
 interface RaceDao {
 
     @Insert
